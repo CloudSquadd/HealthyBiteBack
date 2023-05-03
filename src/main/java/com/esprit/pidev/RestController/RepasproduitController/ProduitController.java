762209f0d84dfd735ccada1e484505b38695e355
NadaYh <katernada.yahmdi@esprit.tn>
2023-05-03 04:15:23 +0200

    integration

--- src/main/java/com/esprit/pidev/RestController/RepasproduitController/ProduitController.java
+++ src/main/java/com/esprit/pidev/RestController/RepasproduitController/ProduitController.java
@@ -15,7 +15,7 @@ import java.util.Set;
 
 @RestController
 @Data
-@CrossOrigin(origins = "*")
+
 @AllArgsConstructor
 @RequestMapping("/api/test")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials="true")
--- src/main/java/com/esprit/pidev/entities/ProduitRepas/Produit.java
+++ src/main/java/com/esprit/pidev/entities/ProduitRepas/Produit.java
@@ -49,9 +49,7 @@ public class Produit implements Serializable {
         }
     }
     private Boolean bloquee=false;
-    @Column(name = "image_data")
-    @Lob
-    private byte[] imageData;
+
 
 
     @Column(name = "image_type")
@@ -60,20 +58,8 @@ public class Produit implements Serializable {
     @Column(name = "image_path")
     private String imagePath;
 
-    public String getImageBase64() {
-        if (imageData == null) {
-            return null;
-        }
-        return Base64.getEncoder().encodeToString(imageData);
-    }
 
-    public void setImageBase64(String imageBase64) {
-        if (imageBase64 == null) {
-            imageData = null;
-        } else {
-            imageData = Base64.getDecoder().decode(imageBase64);
-        }
-    }
+
 
 
     @ManyToOne
